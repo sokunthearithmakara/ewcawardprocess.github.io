1b11358fad98bb4c313cf3b972ef79544ec1ed65
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-26 09:02:52 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
-    $('#page-description').replaceWith('<p class="text-left" id="page-description">Please refer to the email from EWC Program Staff for the instructions to complete this form. If you have any questions, please email the appropriate EWC Program Staff.</p>');
-});
-alert("Hello again");
+//$(document).ready(function() {
+//    $('#page-description').replaceWith('<p class="text-left" id="page-description">Please refer to the email from EWC Program Staff for the instructions to complete this form. If you have any questions, please email the appropriate EWC Program Staff.</p>');
+//});
+//alert("Hello again");
