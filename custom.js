58519c1297e409457aace400325a250920f92fa8
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-26 01:09:36 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,5 @@
 alert("Page start");
+     $('#businessSecondEmailType','#homeSecondEmailType').attr('disabled', true);
 $(document).ready(function() {
      $('#businessSecondEmailType','#homeSecondEmailType').attr('disabled', true);
   alert("Hello");
