8ccd96b5e4d203d810fb5139c22d11834eff58d1
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-27 12:11:31 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -4,27 +4,27 @@ $(document).ready(function() {
     $("#personal2").attr('disabled', true);
     $("#business2").attr('disabled', true);
     $("#homeSecondEmailType").closest('fieldset').addClass("text-muted");
-    $("#personal2").closest('fieldset').addClass("text-muted");
+    $("#personal2").closest('legend').addClass("text-muted");
     $("[name='contactInfo.secondEmail']").change(function() {
         if ($(this).val() != "") {
             $("#homeSecondEmailType").attr('disabled', false).prop('required', true);
             $("#businessSecondEmailType").attr('disabled', false);
-            $("#homeSecondEmailType").closest('fieldset').addClass("text-muted");
+            $("#homeSecondEmailType").closest('legend').addClass("text-muted");
         } else {
             $("#homeSecondEmailType").attr('disabled', true).prop('required', false).prop('checked', false);
             $("#businessSecondEmailType").attr('disabled', true).prop('checked', false);
-            $("#homeSecondEmailType").closest('fieldset').removeClass("text-muted");
+            $("#homeSecondEmailType").closest('legend').removeClass("text-muted");
         }
     });
     $("[name='emergencyContactInfo.secondaryTelephone']").change(function() {
         if ($(this).val() != "") {
             $("#personal2").attr('disabled', false).prop('required', true);
             $("#business2").attr('disabled', false);
-            $("#personal2").closest('fieldset').addClass("text-muted");
+            $("#personal2").closest('legend').addClass("text-muted");
         } else {
             $("#personal2").attr('disabled', true).prop('required', false).prop('checked', false);
             $("#business2").attr('disabled', true).prop('checked', false);
-            $("#personal2").closest('fieldset').removeClass("text-muted");
+            $("#personal2").closest('legend').removeClass("text-muted");
         }
     });
     $("input[type='text']").bind('keyup blur',function() { 
