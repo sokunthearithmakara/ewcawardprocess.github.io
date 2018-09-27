ec102b5ceedc7b9150b847fe3c77c840b5a74030
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-26 18:35:29 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    $('#page-description').replaceWith('<p class="text-left" id="page-description">Please refer to the email from EWC Program Staff for the instructions to complete this form. If you have any questions, please email the appropriate EWC Program Staff.</p>');
+    $('#homeSecondEmailType','#businessSecondEmailType','#personal2','#business2').attr('disabled', true);
 });
