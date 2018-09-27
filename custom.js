ab837d73f31ad2303ea80dc7c3d51f4a4c713a96
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-26 18:49:52 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
-$("input[type=radio]#homeSecondEmailType").attr('disabled', true);
-$("#businessSecondEmailType","#personal2","#business2").attr('disabled', true);
+    $("#homeSecondEmailType").attr('disabled', true);
+    $("#businessSecondEmailType").attr('disabled', true);
+    $("#personal2").attr('disabled', true);
+    $("#business2").attr('disabled', true);
+  $(function() {
+    
+  });
 });
