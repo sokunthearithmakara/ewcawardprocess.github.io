1b5fd992109e1d53411284d6e45517e96b11a709
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-26 18:38:24 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    $('#homeSecondEmailType','#businessSecondEmailType','#personal2','#business2').attr('disabled', true);
+    $('#homeSecondEmailType','#businessSecondEmailType','#personal2','#business2').prop('disabled', true);
 });
