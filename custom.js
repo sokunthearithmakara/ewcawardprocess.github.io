92352b38ffe5d3d025975b89ff8a98230b69a094
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-27 13:13:22 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //make sure secondary email type and secondary phone type are disabled when the secondary email and secondary phone are empty.
     $("#homeSecondEmailType").attr('disabled', true);
     $("#businessSecondEmailType").attr('disabled', true);
     $("#personal2").attr('disabled', true);
@@ -10,10 +11,14 @@ $(document).ready(function() {
             $("#homeSecondEmailType").attr('disabled', false).prop('required', true);
             $("#businessSecondEmailType").attr('disabled', false);
             $("#homeSecondEmailType").closest('fieldset').removeClass("text-muted");
+            var requiredStar = '<span class="required_field">*</span>';
+            $(requiredStar).appendTo($("#homeSecondEmailType").closest('fieldset').find("legend"));
+
         } else {
             $("#homeSecondEmailType").attr('disabled', true).prop('required', false).prop('checked', false);
             $("#businessSecondEmailType").attr('disabled', true).prop('checked', false);
             $("#homeSecondEmailType").closest('fieldset').addClass("text-muted");
+            $("#homeSecondEmailType").closest('fieldset').find("legend span").replaceWith("");
         }
     });
     $("[name='emergencyContactInfo.secondaryTelephone']").change(function() {
@@ -27,6 +32,7 @@ $(document).ready(function() {
             $("#personal2").closest('fieldset').addClass("text-muted");
         }
     });
+    // Limit characters to English characters, numbers, space, and special characters; otherwise, remove the restricted characters as entered.
     $("input[type='text']").bind('keyup blur',function() { 
             $(this).val($(this).val().replace(/[^0-9A-Za-z!;"?_@<>=#$%&'()*+,-./\[\]\ ]/g,''))
         });
