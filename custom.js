389901003366548e1964fd56e7e862768fce9ad0
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-26 19:19:04 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -9,7 +9,16 @@ $(document).ready(function() {
             $("#businessSecondEmailType").attr('disabled', false);
         } else {
             $("#homeSecondEmailType").attr('disabled', true).prop('required', false).prop('checked', false);
-            $("#businessSecondEmailType").attr('disabled', true);
+            $("#businessSecondEmailType").attr('disabled', true).prop('checked', false);
+        }
+    });
+    $("[name='emergencyContactInfo.secondaryTelephone']").change(function() {
+        if ($(this).val() != "") {
+            $("#personal2").attr('disabled', false).prop('required', true);
+            $("#business2").attr('disabled', false);
+        } else {
+            $("#personal2").attr('disabled', true).prop('required', false).prop('checked', false);
+            $("#business2").attr('disabled', true).prop('checked', false);
         }
     });
 });
