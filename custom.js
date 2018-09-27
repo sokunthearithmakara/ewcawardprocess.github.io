6e1a79b736f9f3f1e33994a369f9ffb5be6f2f5a
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-27 12:23:44 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -3,28 +3,28 @@ $(document).ready(function() {
     $("#businessSecondEmailType").attr('disabled', true);
     $("#personal2").attr('disabled', true);
     $("#business2").attr('disabled', true);
-    //$("#homeSecondEmailType").closest('fieldset').addClass("text-muted");
-    //$("#personal2").closest('fieldset').addClass("text-muted");
+    $("#homeSecondEmailType").closest('fieldset').addClass("text-muted");
+    $("#personal2").closest('fieldset').addClass("text-muted");
     $("[name='contactInfo.secondEmail']").change(function() {
         if ($(this).val() != "") {
             $("#homeSecondEmailType").attr('disabled', false).prop('required', true);
             $("#businessSecondEmailType").attr('disabled', false);
-            $("#homeSecondEmailType").closest('fieldset').addClass("text-muted");
+            $("#homeSecondEmailType").closest('fieldset').removeClass("text-muted");
         } else {
             $("#homeSecondEmailType").attr('disabled', true).prop('required', false).prop('checked', false);
             $("#businessSecondEmailType").attr('disabled', true).prop('checked', false);
-            $("#homeSecondEmailType").closest('fieldset').removeAttr("class");
+            $("#homeSecondEmailType").closest('fieldset').addClass("text-muted");
         }
     });
     $("[name='emergencyContactInfo.secondaryTelephone']").change(function() {
         if ($(this).val() != "") {
             $("#personal2").attr('disabled', false).prop('required', true);
             $("#business2").attr('disabled', false);
-            $("#personal2").closest('fieldset').addClass("text-muted");
+            $("#personal2").closest('fieldset').removeClass("text-muted");
         } else {
             $("#personal2").attr('disabled', true).prop('required', false).prop('checked', false);
             $("#business2").attr('disabled', true).prop('checked', false);
-            $("#personal2").closest('fieldset').removeAttr("class");
+            $("#personal2").closest('fieldset').addClass("text-muted");
         }
     });
     $("input[type='text']").bind('keyup blur',function() { 
