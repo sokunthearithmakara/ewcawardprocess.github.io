a274d7117a8bcf2bfbd435d00f9f0fdcb5d308ce
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-26 19:03:28 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
-    $("#homeSecondEmailType","#businessSecondEmailType").prop('disabled', true);
-//    $("#businessSecondEmailType").attr('disabled', true);
-//    $("#personal2").attr('disabled', true);
-//    $("#business2").attr('disabled', true);
-  $(function() {
-    
-  });
+    $("#homeSecondEmailType").attr('disabled', true);
+    $("#businessSecondEmailType").attr('disabled', true);
+    $("#personal2").attr('disabled', true);
+    $("#business2").attr('disabled', true);
+    $('[name=contactInfo.secondEmail]').change(function() {
+        if (this.val() != '') {
+            $("#homeSecondEmailType").attr('disabled', false).prop('required', true);
+            $("#businessSecondEmailType").attr('disabled', false);
+        } else {
+            $("#homeSecondEmailType").attr('disabled', true).prop('required', false).prop('checked', false);
+            $("#businessSecondEmailType").attr('disabled', true);
+        }
+    });
 });
