7fb953d29babffdbee7c47933d4f724676fab0b6
ewcawardprocess <43083692+ewcawardprocess@users.noreply.github.com>
2018-09-27 14:21:48 -1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -46,5 +46,5 @@ $(document).ready(function() {
             $(this).val($(this).val().replace(/[^0-9()#/+-. ]/g,''))
         });
     //Remove tooltip for mobile phone field
-    $("input[type='tel']").removeAttr("data-original-title");
+    $(".mobileTelephone input").removeAttr("data-original-title");
 });
